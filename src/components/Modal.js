be17dbf55185e4e2ce789bed549bc714905d216d
Nishant Dixit <nd221619@gmail.com>
2023-06-03 17:05:50 +0530

    final changes[6]

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,7 @@ import { createPortal } from 'react-dom'
 const Modal = ({onClose,children,}) => {
   return createPortal(
     <div className='bg-black/30 sm:h-screen fixed inset-0 w-screen'>
-    <div className='md:w-1/4 md:h-1/3 rounded-xl p-2 flex flex-col fixed inset-x-0 inset-y-0 z-50  m-auto items-center  bg-white drop-shadow-xl'>
+    <div className='md:w-1/4 h-1/3 rounded-xl p-2  flex flex-col fixed inset-x-0 inset-y-0 z-50  m-auto items-center  bg-white drop-shadow-xl'>
         <button className='text-xl   px-2 rounded-2xl text-black/40   ml-auto' onClick={()=>onClose(false)}>x</button>
         <hr />
         <div className='p-2'>{children}</div>
